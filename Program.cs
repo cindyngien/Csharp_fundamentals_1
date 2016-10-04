078cf61286574ed9b06c6f258971516aa577c3be
Cindy Ngien <cindyngien@gmail.com>
2016-10-03 17:21:56 -0700

    first commit - fizz buzz!

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    break;
+                }
+                else if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+            }
+        }
+    }
+}
